ec118703ad9a61970b4190e5204637829c573c95
Rayhan Islam <your-email@example.com>
2025-01-29 01:12:37 +0600

    last please

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+      "./src/**/*.{html,js,jsx,ts,tsx}" // Includes all HTML and JS/TS files in the src folder
+    ],
+    theme: {
+      extend: {
+        
+      }, // Add customizations here
+    },
+    plugins: [], // Add plugins if needed
+  };
+  
